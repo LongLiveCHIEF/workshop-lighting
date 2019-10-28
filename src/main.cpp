04180445666d41f2bd25c5fb0b452bd45b7beed7
Brian vanderbusch <brian.vanderbusch@gmail.com>
2019-10-28 13:56:25 -0500

    add blinking for feedback

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <WiFiManager.h>
+#include <Arduino.h>
+#include <Ticker.h>
+
 WiFiManager wm;
+Ticker blinker;
+
+void blink(){
+  int state = digitalRead(LED_BUILTIN);
+  digitalWrite(LED_BUILTIN, !state);
+}
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  // blink every 0.3s
+  blinker.attach(0.3, blink);
+
+  // setup wifi AP
   WiFi.mode(WIFI_STA);
 
   Serial.begin(9600);
